091b0ae81211ff33a144ecfc4aa40523173907c5
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2024-05-14 00:41:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -103,12 +103,7 @@ checkLoginStatus();
           <div class="footer-wrap">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
               <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©
-                <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2021</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best
-                <a href="https://www.bootstrapdash.com/" target="_blank">
-                  Bootstrap dashboard
-                </a>
-                templates</span>
+                Carlos Hilado Memorial State University 2024</span>
             </div>
           </div>
         </footer>
