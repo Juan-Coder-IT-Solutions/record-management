9d715864277f748da4d0ab980da1bbc3f921fe4d
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2024-05-14 09:19:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,6 @@ $user_id = $_SESSION['rm_user_id'];
   <link rel="shortcut icon" href="images/chmsu.png" />
 
 </head>
-
 <body>
   <div class="container-scroller">
     <!-- partial:partials/_horizontal-navbar.html -->
@@ -59,31 +58,31 @@ $user_id = $_SESSION['rm_user_id'];
       <nav class="bottom-navbar">
         <div class="container">
           <ul class="nav page-navigation">
-            <li class="nav-item">
+            <li class="nav-item <?= ($page == "" || $page == "dashboard"? "active":"") ?>">
               <a class="nav-link" href="./">
                 <i class="mdi mdi-file-document-box menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
               </a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= ($page == "programs"? "active":"") ?>">
               <a href="index.php?page=programs" class="nav-link">
                 <i class="mdi mdi-cube-outline menu-icon"></i>
                 <span class="menu-title">Programs</span>
               </a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= ($page == "tasks"? "active":"") ?>">
               <a href="index.php?page=tasks" class="nav-link">
                 <i class="mdi mdi-chart-gantt menu-icon"></i>
                 <span class="menu-title">Task</span>
               </a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= ($page == "users"? "active":"") ?>">
               <a href="index.php?page=users" class="nav-link">
                 <i class="mdi mdi-account-multiple-outline menu-icon"></i>
                 <span class="menu-title">Users</span>
               </a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= ($page == "profile"? "active":"") ?>">
               <a href="index.php?page=profile" class="nav-link">
                 <i class="mdi mdi-account-circle menu-icon"></i>
                 <span class="menu-title">Profile </span>
